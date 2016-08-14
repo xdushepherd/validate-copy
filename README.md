45d1233b61a8615cd1b42067078dc3866060511f
xdushepherd <874654098@qq.com>
2016-08-14 15:49:57 +0800

    增加更灵活的借口，方便用户添加新的验证规则和验证项目

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # validate-copy
 ###一个表单验证插件，又一个轮子，在复制轮子的过程中，提高下自己
+
+###用法
+
+```javascript
+//初始化
+	validator = new FormValidator(document.forms[0],[{identy:".heihei",rule:"required",message:"不能为空"},
+			                       {identy:"#heihei1",rule:"lt:20",message:"字符串长度不能多于20"},
+			                       {identy:"heihei2",rule:"gt:6",message:"字符串长度不能少于6"}]
+			             ]);
+//添加新的验证项目
+    validator.addValidateOption([{identy:".heihei",rule:"required",message:"不能为空"},
+			                       {identy:"#heihei1",rule:"lt:20",message:"字符串长度不能多于20"},
+			                       {identy:"heihei2",rule:"gt:6",message:"字符串长度不能少于6"}]
+			                   );
+//添加新的验证规则
+	validator.addValidateItem(phoneNumber,function(value){
+								return /^1[3|5|7|8]\d{9}$/.test(value)
+							})
+
+```
--- validate.js
+++ validate.js
@@ -1,7 +1,7 @@
 /*
- *
- *
- *
+ * 这是一个基于原声JavaScript语言编写的表单验证插件，
+ * 未考虑相关浏览器的兼容性问题，旨在实践JavaScript
+ * 基础知识及设计模式
  */
 
 
@@ -14,36 +14,37 @@
   }
   
   //option = [{name/class/id:"",rule:"",message:"heiheihei"},{},{},{}]
+  var classRex = /^\.\w/i;
+  var idRex = /^\#\w/i;
 
   var FormValidator = function(formNameorNode,option) {
   	this.options = {};
   	this.valified = true;
   	this.message = "";
+    that = this;
 
   	//TODO 使用更灵活的form对象
   	this.form = this.findForm(formNameorNode);
 
-  	this._addValidateOption(option||[]);
+  	this._addValidateOptions(option||[]);
 
   	//匿名函数中的this值不是FormValidator的实例
-  	this.form.onsubmit = (function(that) {
-  			return function(event) {
+  	this.form.onsubmit = function(event) {
 	  	  		if(!that._validateForm()){
 	  	  			console.log(that.message)
 	                event.preventDefault();
 	            }
   			}
-  	  	})(this);
   }
 
-  FormValidator.prototype._addValidateOption = function(option) {
+  FormValidator.prototype._addValidateOptions = function(option) {
   	for (var i = 0; i < option.length; i++) {
   		this.options[option[i].identy] = option[i];
   	}
   }
 
   FormValidator.prototype.findForm = function(form) {
-  	return doc.forms[form];
+  	return typeof form == 'object' ? form : doc.forms[form];
   }
 
   FormValidator.prototype._validateForm = function() {
@@ -53,8 +54,8 @@
   		total_rule = this.options[key].rule.split(":");
   		var rule = total_rule[0];
   		var length = total_rule[1] || "";
-  		if(!this._hooks[rule].call(this,this.form[key].value,length)){
-  			this.message = this.options[key].message
+  		if(!this._hooks[rule].call(this,this.findInput(key).value,length)){
+  			this.message = key + ": " +this.options[key].message
   			this.valified = false;
   			break;
   		}
@@ -62,19 +63,36 @@
   	return this.valified;
   }
 
+  FormValidator.prototype.findInput = function(identy) {
+    if (classRex.test(identy) ||idRex.test(identy)) {
+      console.log("The identy is an ID or class")
+      return doc. querySelector(identy) || null;
+    }else{
+      return this.form[identy] || null;
+    }
+  };
+
+  FormValidator.prototype.addValidateOption = function(option) {
+    this._addValidateOptions(option);
+  };
+
+  FormValidator.prototype.addValidateItem = function(name,callback) {
+    this._hooks[name] = callback;
+  };
+
   FormValidator.prototype._hooks = {
-	required: function(value) {
-		return (value != null && value != "" && value !== undefined)
-	},
-	lt: function(value,length) {
-		return (value.length <= length);
-	},
-	gt: function(value,length) {
-		return (value.length >= length)
-	},
-	eq: function(value,length) {
-		return (value != null && value != "" && value !== undefined)
-	}
+  	required: function(value) {
+  		return (value != null && value != "" && value !== undefined)
+  	},
+  	lt: function(value,length) {
+  		return (value.length <= length);
+  	},
+  	gt: function(value,length) {
+  		return (value.length >= length)
+  	},
+  	eq: function(value,length) {
+  		return (value.length === length)
+  	}
   }
 
   win.FormValidator = FormValidator;
